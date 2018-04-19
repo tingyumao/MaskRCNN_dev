d349cad186c979b8c3d94dc07db01be7d547240f
tingyumao <gtym1177@gmail.com>
2018-04-19 14:45:30 -0400

    fix buggg

--- model.py
+++ model.py
@@ -1927,22 +1927,18 @@ class MaskRCNN():
                 [target_class_ids, mrcnn_class_logits, active_class_ids])
             bbox_loss = KL.Lambda(lambda x: mrcnn_bbox_loss_graph(*x), name="mrcnn_bbox_loss")(
                 [target_bbox, target_class_ids, mrcnn_bbox])
-            # mask_loss = KL.Lambda(lambda x: mrcnn_mask_loss_graph(*x), name="mrcnn_mask_loss")(
-            #     [target_mask, target_class_ids, mrcnn_mask])
+            mask_loss = KL.Lambda(lambda x: mrcnn_mask_loss_graph(*x), name="mrcnn_mask_loss")(
+                [target_mask, target_class_ids, mrcnn_mask])
 
             # Model
             inputs = [input_image, input_image_meta,
                       input_rpn_match, input_rpn_bbox, input_gt_class_ids, input_gt_boxes, input_gt_masks]
             if not config.USE_RPN_ROIS:
                 inputs.append(input_rois)
-            # outputs = [rpn_class_logits, rpn_class, rpn_bbox,
-            #            mrcnn_class_logits, mrcnn_class, mrcnn_bbox, mrcnn_mask,
-            #            rpn_rois, output_rois,
-            #            rpn_class_loss, rpn_bbox_loss, class_loss, bbox_loss, mask_loss]
             outputs = [rpn_class_logits, rpn_class, rpn_bbox,
                        mrcnn_class_logits, mrcnn_class, mrcnn_bbox, mrcnn_mask,
                        rpn_rois, output_rois,
-                       rpn_class_loss, rpn_bbox_loss, class_loss, bbox_loss]
+                       rpn_class_loss, rpn_bbox_loss, class_loss, bbox_loss, mask_loss]
             model = KM.Model(inputs, outputs, name='mask_rcnn')
         else:
             # Network Heads
